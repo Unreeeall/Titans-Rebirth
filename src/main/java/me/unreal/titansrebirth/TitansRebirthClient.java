bf4468c11399f22eabe5762b364161bfb2d21956
Unreal <unrealfinn@gmail.com>
2025-04-20 14:51:29 +0200

    Added the client class

---
+++ src/main/java/me/unreal/titansrebirth/TitansRebirthClient.java
@@ -0,0 +1,10 @@
+package me.unreal.titansrebirth;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TitansRebirthClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
