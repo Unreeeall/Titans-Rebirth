e8c527154a396b803826b8dd6753189a1f53a112
Unreal <unrealfinn@gmail.com>
2025-04-20 14:32:37 +0200

    Initial Commit

---
+++ src/main/java/me/unreal/titansrebirth/TitansRebirthDataGenerator.java
@@ -0,0 +1,11 @@
+package me.unreal.titansrebirth;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TitansRebirthDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
