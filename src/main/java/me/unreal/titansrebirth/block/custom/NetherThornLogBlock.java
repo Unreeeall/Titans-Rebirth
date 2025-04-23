754dc933cb41dcd04cffb45614e5238eff1534e5
Unreal <unrealfinn@gmail.com>
2025-04-23 21:03:20 +0200

    Fixed block not rotating

--- src/main/java/me/unreal/titansrebirth/block/custom/NetherThornLogBlock.java
+++ src/main/java/me/unreal/titansrebirth/block/custom/NetherThornLogBlock.java
@@ -1,20 +1,61 @@
 package me.unreal.titansrebirth.block.custom;
 
 import net.minecraft.block.*;
+import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.EnumProperty;
+import net.minecraft.util.BlockRotation;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.BlockView;
 
 public class NetherThornLogBlock extends Block {
-    private static final VoxelShape SHAPE = Block.createCuboidShape(0, 0, 0, 16, 16, 16);
+    public static final EnumProperty<Direction.Axis> AXIS = EnumProperty.of("axis", Direction.Axis.class);
+    private static final VoxelShape SHAPE = Block.createCuboidShape(0, 0, 0, 16, 16, 16); // Full block collision
 
     public NetherThornLogBlock(Settings settings) {
         super(settings);
+        // Set default state to Y-axis (vertical)
+        this.setDefaultState(this.getStateManager().getDefaultState().with(AXIS, Direction.Axis.Y));
     }
 
     @Override
-    protected VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+    public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
         return SHAPE;
     }
 
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(AXIS); // Add AXIS property
+    }
+
+    @Override
+    public BlockState getPlacementState(ItemPlacementContext ctx) {
+        // Set the axis based on the clicked face
+        return this.getDefaultState().with(AXIS, ctx.getSide().getAxis());
+    }
+
+    @Override
+    public BlockState rotate(BlockState state, BlockRotation rotation) {
+        // Handle rotation (e.g., for structure rotation)
+        return rotatePillar(state, rotation);
+    }
+
+    public static BlockState rotatePillar(BlockState state, BlockRotation rotation) {
+        switch (rotation) {
+            case COUNTERCLOCKWISE_90:
+            case CLOCKWISE_90:
+                switch (state.get(AXIS)) {
+                    case X:
+                        return state.with(AXIS, Direction.Axis.Z);
+                    case Z:
+                        return state.with(AXIS, Direction.Axis.X);
+                    default:
+                        return state;
+                }
+            default:
+                return state;
+        }
+    }
 }
